8cbe64d9e4c15aada4e5ba6a8d96ef4bb61a0ba0
alexTver33 <79607025659@yandex.ru>
2023-02-03 19:12:38 +0300

    написанна программа, решающая поставленную задачу

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿static string[] GetLessThan4(string[] input)
+{
+    var counter = 0;
+    var newArray = new string[input.Length];
+    foreach(var element in input)
+    {
+        if (element.Length <= 3) 
+        {
+            newArray[counter] = element;
+            counter++;
+        }
+        
+    }
+    Array.Resize<string>(ref newArray, counter);
+
+    return newArray;
+}
+
+var array = new string[]{"hello", "2", "world", ":-)"};
+
+var output = GetLessThan4(array);
+
+foreach(var e in output){
+    Console.Write(e + " ");
+}
